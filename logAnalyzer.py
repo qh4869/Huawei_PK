6447412e55c6f3aee29b59c3d1fc73725e2c722f
qh4869 <WDqiuhao@163.com>
2021-04-21 20:24:07 +0800

    添加log分析器，几个典型的test可执行文件

--- logAnalyzer.py
+++ logAnalyzer.py
@@ -4,6 +4,10 @@ import sys
 
 player0_profit = []
 player1_profit = []
+player0_income = [] # 收入
+player1_income = []
+player0_req = [] # 成功交易次数
+player1_req = []
 
 def get_profit(input_lines):
 	for line in input_lines:
@@ -13,16 +17,86 @@ def get_profit(input_lines):
 			player1_profit.append(int(value[4]))
 
 
+def get_income_and_req(input_lines):
+	for line in input_lines:
+		value = line.strip().split()
+		if len(value)==2 and value[0]=='Day':
+			daily_income_0 = 0
+			daily_income_1 = 0
+			daily_req_0 = 0
+			daily_req_1 = 0
+		elif len(value)==2:
+			value_0 = int(value[0])
+			value_1 = int(value[1])
+
+			if value_0 == -1 and value_1 == -1:
+				succ_0 = False
+				succ_1 = False
+			elif value_0 == -1 and value_1 != -1:
+				succ_0 = False
+				succ_1 = True
+			elif value_0 != -1 and value_1 == -1:
+				succ_0 = True
+				succ_1 = False
+			elif value_0 < value_1:
+				succ_0 = True
+				succ_1 = False
+			elif value_0 > value_1:
+				succ_0 = False
+				succ_1 = True
+			else:
+				succ_0 = True
+				succ_1 = True
+
+			if len(player0_income)==738 and succ_1:
+				a = 1
+
+			if succ_0:
+				daily_income_0 += value_0
+				daily_req_0 += 1
+
+			if succ_1:
+				daily_income_1 += value_1
+				daily_req_1 += 1
+
+		elif len(value)==5 and value[0]=='player0' and value[1]=='player1(Current':
+			# if player0_income and player1_income:
+			# 	player0_income.append(player0_income[-1] + daily_income_0)
+			# 	player1_income.append(player1_income[-1] + daily_income_1)
+			# 	player0_req.append(player0_req[-1] + daily_req_0)
+			# 	player1_req.append(player1_req[-1] + daily_req_1)
+			# else:
+				player0_income.append(daily_income_0)
+				player1_income.append(daily_income_1)
+				player0_req.append(daily_req_0)
+				player1_req.append(daily_req_1)
+
+
 def main(argv):
 	with open(argv[1], 'r') as file:
 		input_lines = file.readlines()
 
 	get_profit(input_lines)
+	get_income_and_req(input_lines)
 
 	plt.figure()
-	plt.plot(player0_profit, color='red', label='player0')
-	plt.plot(player1_profit, color='blue', label='player1')
+	plt.plot(player0_profit, color='red', label='player0',alpha=0.7)
+	plt.plot(player1_profit, color='blue', label='player1',alpha=0.7)
+	plt.title('Total profit')
 	plt.legend()
+
+	plt.figure()
+	plt.plot(player0_income, color='red', label='player0', alpha=0.7, zorder=1)
+	plt.plot(player1_income, color='blue', label='player1', alpha=0.7, zorder=0)
+	plt.title('Income Each Day')
+	plt.legend()
+
+	plt.figure()
+	plt.plot(player0_req, color='red', label='player0',alpha=0.7)
+	plt.plot(player1_req, color='blue', label='player1',alpha=0.7)
+	plt.title('Obtained Req. Each Day')
+	plt.legend()
+
 	plt.show()
 
 if __name__ == '__main__':
